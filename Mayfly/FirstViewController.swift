a9f442a8b232e21d9d4e6ead1d78e5f2d41c977a
Kushal <kushal.nigam1@gmail.com>
2014-07-20 00:31:10 -0400

    Initial Commit

---
+++ Mayfly/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mayfly
+//
+//  Created by Kushal Nigam on 7/20/14.
+//  Copyright (c) 2014 Kushal Nigam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mayfly/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mayfly
+//
+//  Created by Kushal Nigam on 7/20/14.
+//  Copyright (c) 2014 Kushal Nigam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
