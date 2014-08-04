a7bbfb99ec60a454868ab1675f78b9d3d847e4db
Atul Nigam <atul.nigam@ge.com>
2014-08-03 21:15:18 -0400

    added HttpHelper class

---
+++ Mayfly/HttpHelper.swift
@@ -0,0 +1,70 @@
+//
+//  HttpHelper.swift
+//  Mayfly
+//
+//  Created by Atul Nigam on 8/3/14.
+//  Copyright (c) 2014 Kushal Nigam. All rights reserved.
+//
+
+import Foundation
+
+
+protocol HTTPHelperDelegate{
+    
+    func didReceiveHTTPResponseResults(results: NSDictionary)
+    
+    
+}
+
+
+class HTTPHelper{
+    
+    
+    init(){}
+    
+    func httpPostJSON(uri: String, data:NSDictionary, delegate: HTTPHelperDelegate? ){
+        
+        let url: NSURL = NSURL(string: uri)
+        
+        var request = NSMutableURLRequest(URL:  url)
+        
+        var err: NSError?
+        
+        request.HTTPMethod = "POST"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(data, options: nil, error: &err)
+        
+        if(err?) {
+            // If there is an error parsing JSON, print it to the console
+            println("Error in creating dataWithJSONObject:  \(err!.localizedDescription)")
+        }
+        
+        var session = NSURLSession.sharedSession()
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error-> Void
+            in
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var responseError: NSError?
+            var response = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &responseError) as NSDictionary
+            
+            if(responseError?) {
+                // If there is an error parsing JSON, print it to the console
+                println("Error in creating JSONObjectWithData: \(responseError!.localizedDescription)")
+            } else {
+                println("Response received successfully \(response.debugDescription)")
+            }
+            // Now send the JSAON result to our delegate object
+            delegate?.didReceiveHTTPResponseResults(response)
+            })
+        task.resume()
+        
+    }
+    
+}
+
+
+
