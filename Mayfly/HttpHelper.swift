9a97d2febbdbf68050b8054e4abbda7f6bc581c9
Atul Nigam <atul.nigam@ge.com>
2014-08-03 23:08:41 -0400

    add HttpDelete function

--- Mayfly/HttpHelper.swift
+++ Mayfly/HttpHelper.swift
@@ -11,14 +11,12 @@ import Foundation
 
 protocol HTTPHelperDelegate{
     
-    func didReceiveHTTPResponseResults(results: NSDictionary)
-    
+    func didReceiveHTTPResponseResults(results: NSDictionary) 
     
 }
 
 
 class HTTPHelper{
-    
     
     init(){}
     
@@ -63,6 +61,41 @@ class HTTPHelper{
         task.resume()
         
     }
+    
+    func httpDelete(uri: String, delegate: HTTPHelperDelegate? ){
+        
+        let url: NSURL = NSURL(string: uri)
+        
+        var request = NSMutableURLRequest(URL:  url)
+        
+        var err: NSError?
+        
+        request.HTTPMethod = "DELETE"
+        
+        var session = NSURLSession.sharedSession()
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error-> Void
+            in
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var responseError: NSError?
+            
+            var response = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &responseError) as NSDictionary
+            
+            if(responseError?) {
+                // If there is an error parsing JSON, print it to the console
+                println("Error in creating JSONObjectWithData: \(responseError!.localizedDescription)")
+            } else {
+                println("Response received successfully \(response.debugDescription)")
+            }
+            // Now send the JSAON result to our delegate object
+            delegate?.didReceiveHTTPResponseResults(response)
+            })
+        task.resume()
+        
+    }
+    
     
 }
 
